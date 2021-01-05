a553b10b2d16f1f0f7daf1ea5dd795783ff87f44
Tuukka Virtanen <duukkis@gmail.com>
2021-01-05 17:35:29 +0200

    list names

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const App = (props) => {
+const App = ( props ) => {
   const [ names, setNames ] = useState([])
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/names')
-      .then(response => {
-        console.log(response.data)
-        setNames(response.data)
+      .get( 'http://localhost:3001/names' )
+      .then( response => {
+        console.log( response.data )
+        setNames( response.data )
       })
   }, [])
 
   return (
     <div>
       <h1>Names app</h1>
+      <ul>
+        { names.map( name =>
+          <li key={ name.name }>{ name.name }<br></br>amount: { name.amount }</li>
+        )}
+      </ul>
     </div>
   )
 }
