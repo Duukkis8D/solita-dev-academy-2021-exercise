1edbf3b34a81ea5d41f4b4cd29471fd5254b0fbe
Tuukka Virtanen <duukkis@gmail.com>
2021-01-10 23:51:43 +0200

    getting ready for backend development

--- src/services/nameService.js
+++ src/services/nameService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getNames = () => {
-    return axios.get( 'http://localhost:3001/names' )
+    return axios.get( 'http://localhost:3001/api/names' )
 }
 
 export default { getNames }
